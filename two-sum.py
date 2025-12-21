c19da630b6caf69d0edb20b192be646eccb9c640
imadbourouche <ji_bourouche@esi.dz>
2025-12-21 14:46:00 +0100

    wip

---
+++ two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        solutions = {}
+        for i in range(len(nums)):
+            current_element = nums[i]
+            if current_element in solutions:
+                return [solutions[current_element], i]
+            else:
+                difference = target - current_element
+                solutions[difference] = i
+        return []
